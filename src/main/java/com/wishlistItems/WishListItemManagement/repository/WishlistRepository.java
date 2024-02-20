3c50b552aba4e951be0eeec405326372945c38f5
rohantastic <rohantastic@outlook.com>
2024-02-20 11:53:50 +0530

    working on repositories and configuration

---
+++ src/main/java/com/wishlistItems/WishListItemManagement/repository/WishlistRepository.java
@@ -0,0 +1,9 @@
+package com.wishlistItems.WishListItemManagement.repository;
+import com.wishlistItems.WishListItemManagement.entities.User;
+import com.wishlistItems.WishListItemManagement.entities.WishlistEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface WishlistRepository extends JpaRepository<WishlistEntity,Long> {
+    List<WishlistEntity> findByUser(User user);
+}
