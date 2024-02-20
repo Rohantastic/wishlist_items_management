aacf8c92c5331e1d8ab9e1b8232c79b59233c9fb
rohantastic <rohantastic@outlook.com>
2024-02-20 11:39:39 +0530

    first commit

---
+++ src/test/java/com/wishlistItems/WishListItemManagement/WishListItemManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wishlistItems.WishListItemManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishListItemManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
